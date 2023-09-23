85f1e5a36c3fdea9143e938067cdbaa7f8fa3528
haqueashraful <139930811+haqueashraful@users.noreply.github.com>
2023-09-23 18:18:53 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+let x = myFunction(40, 35);
+
+function myFunction(a, b) {
+  return a * b;
+}
+console.log(x)
+// celcius function
+function toCelsius(x) {
+    return (5/9) * (x-32);
+  }
+  let value = toCelsius(87);
+
+console.log(value)
